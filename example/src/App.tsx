eb0622c707aa95e5effd963ed138ee42260357f7
Sam Autrey <samautrey7@gmail.com>
2021-03-21 13:01:38 -0500

    fix: horizontal text jumping fix

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,14 +2,14 @@ import * as React from 'react';
 import { StyleSheet, View } from 'react-native';
 // import { Ionicons } from '@expo/vector-icons';
 // import { InputOutline } from 'react-native-input-outline';
-import Showcase from './Showcase';
-// import StandardShowcase from './StandardShowcase';
+// import Showcase from './Showcase';
+import StandardShowcase from './StandardShowcase';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* <StandardShowcase /> */}
-      <Showcase />
+      <StandardShowcase />
+      {/* <Showcase /> */}
     </View>
   );
 }
